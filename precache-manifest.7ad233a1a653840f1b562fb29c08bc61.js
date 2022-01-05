8e9bbd4835c2caa0a83ca6d6691e7ca2b934de61
saidAalshalaby <said.ahmed333312@yahoo.com>
2022-01-05 18:53:25 +0400

    Updates

---
+++ precache-manifest.7ad233a1a653840f1b562fb29c08bc61.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad411a4de2d038a13428361787d66a2",
+    "url": "/GRGMedical/index.html"
+  },
+  {
+    "revision": "d3cc3a7b4a61405402ad",
+    "url": "/GRGMedical/static/css/main.cf0590ae.chunk.css"
+  },
+  {
+    "revision": "740e8bef00d41384f0dd",
+    "url": "/GRGMedical/static/js/2.d87948d5.chunk.js"
+  },
+  {
+    "revision": "7e6fd9936a6e13c0afb9e86b21fe4ff2",
+    "url": "/GRGMedical/static/js/2.d87948d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cc3a7b4a61405402ad",
+    "url": "/GRGMedical/static/js/main.ef38ea07.chunk.js"
+  },
+  {
+    "revision": "1301e23c01d1752d6a1a",
+    "url": "/GRGMedical/static/js/runtime-main.1b9ce1c9.js"
+  },
+  {
+    "revision": "0faaa6fbe2d8886f88fe5b43e15c4e8c",
+    "url": "/GRGMedical/static/media/1920x900-slider.0faaa6fb.png"
+  },
+  {
+    "revision": "c8cb0be571ce5c67b3a1a31dd4a5551b",
+    "url": "/GRGMedical/static/media/Antiseptic.c8cb0be5.png"
+  },
+  {
+    "revision": "db0e67ec79d6454f8a1f2584cf868b96",
+    "url": "/GRGMedical/static/media/Industrial.db0e67ec.png"
+  },
+  {
+    "revision": "304e3405875fa79781af8348f51ba41d",
+    "url": "/GRGMedical/static/media/Mask.304e3405.png"
+  },
+  {
+    "revision": "9e266c594f279d2166b02a867569dada",
+    "url": "/GRGMedical/static/media/img1.9e266c59.png"
+  },
+  {
+    "revision": "9e266c594f279d2166b02a867569dada",
+    "url": "/GRGMedical/static/media/img10.9e266c59.png"
+  },
+  {
+    "revision": "97e7eb168f8a3914d24ff4e3079d367a",
+    "url": "/GRGMedical/static/media/img11.97e7eb16.png"
+  },
+  {
+    "revision": "97e7eb168f8a3914d24ff4e3079d367a",
+    "url": "/GRGMedical/static/media/img12.97e7eb16.png"
+  },
+  {
+    "revision": "8096395a2d8dc6dff9d87fd366022076",
+    "url": "/GRGMedical/static/media/img13.8096395a.png"
+  },
+  {
+    "revision": "442102f48fc1a1da1f7c7e8576138e27",
+    "url": "/GRGMedical/static/media/img14.442102f4.png"
+  },
+  {
+    "revision": "9e266c594f279d2166b02a867569dada",
+    "url": "/GRGMedical/static/media/img2.9e266c59.png"
+  },
+  {
+    "revision": "9e266c594f279d2166b02a867569dada",
+    "url": "/GRGMedical/static/media/img3.9e266c59.png"
+  },
+  {
+    "revision": "97e7eb168f8a3914d24ff4e3079d367a",
+    "url": "/GRGMedical/static/media/img4.97e7eb16.png"
+  },
+  {
+    "revision": "ccc489266627ed2f8fa3c0f0b8b2fdab",
+    "url": "/GRGMedical/static/media/img5.ccc48926.png"
+  },
+  {
+    "revision": "ccc489266627ed2f8fa3c0f0b8b2fdab",
+    "url": "/GRGMedical/static/media/img6.ccc48926.png"
+  },
+  {
+    "revision": "32ad2b06bb46cc022c0e44981916e0bd",
+    "url": "/GRGMedical/static/media/img7.32ad2b06.png"
+  },
+  {
+    "revision": "32ad2b06bb46cc022c0e44981916e0bd",
+    "url": "/GRGMedical/static/media/img8.32ad2b06.png"
+  },
+  {
+    "revision": "9e266c594f279d2166b02a867569dada",
+    "url": "/GRGMedical/static/media/img9.9e266c59.png"
+  },
+  {
+    "revision": "384e19dd8522be0b058e2403656e0817",
+    "url": "/GRGMedical/static/media/imgCover.384e19dd.jpg"
+  },
+  {
+    "revision": "e08487fe342c8410f24386c9ecdbbb27",
+    "url": "/GRGMedical/static/media/logo.e08487fe.png"
+  },
+  {
+    "revision": "d6509e061bf939991aa6e096a8f91222",
+    "url": "/GRGMedical/static/media/offer.d6509e06.png"
+  }
+]);
